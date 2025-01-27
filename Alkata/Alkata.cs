00529ff27c0a197a64b81c02c8fa4b4319d85cde
Starman <vicentetavareslima@gmail.com>
2025-01-27 22:10:42 +0000

    did some shit

--- Alkata/Alkata.cs
+++ Alkata/Alkata.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using NetShaderc;
+using OpenTK.Graphics.OpenGL4;
+using System.Text;
 namespace Alkata
 {
 	/// <summary>
@@ -21,7 +23,7 @@ namespace Alkata
 
 		public Alkata FromFile(FileStream fs)
 		{
-			Span<byte> buffer = new byte[(int)fs.Length];
+			Span<byte> buffer = new byte[20];
 			fs.Read(buffer);
 			for (int i = 0; i < Magic.Length; i++)
 			{
@@ -31,11 +33,48 @@ namespace Alkata
 				}
 			}
 
+			var version = BitConverter.ToInt64(buffer[8..16]);
+
+			if(version != 1)
+			{
+				throw new InvalidDataException("Invalid Alkata version");
+			}
+
+			var shaderCount = BitConverter.ToInt32(buffer[16..20]);
+			var shaders = new ShaderDef[shaderCount];
+			for (int i = 0; i < shaderCount; i++)
+			{
+				var buffr = new byte[sizeof(uint)];
+				fs.Read(buffr, 0, sizeof(uint));
+				shaders[i].Kind = (ShaderKind)BitConverter.ToUInt32(buffr[0..sizeof(uint)]);
+				buffr = new byte[sizeof(int)];
+				shaders[i].Uniforms = new ShaderDef.Uniform[fs.Read(buffr, 0, sizeof(int))];
+				for (int s = 0; s < shaders[i].Uniforms.Length; s++)
+				{
+					buffr = new byte[sizeof(int)];
+					char[] bufs = new char[fs.Read(buffr, 0, sizeof(int))];
+
+					shaders[i].Uniforms[s].Name = bufs.ToString() ?? throw new NullReferenceException();
+				}
+				buffr = new byte[sizeof(uint)];
+				// do position and size
+
+			}
+
 		}
 
-		static string[] GetUniforms()
+		struct ShaderDef
 		{
+			public struct Uniform
+			{
+				public string Name;
+			}
 
+			public ShaderKind Kind;
+			public Uniform[] Uniforms;
+			public uint Position;
+			public uint Size;
 		}
+
 	}
 }

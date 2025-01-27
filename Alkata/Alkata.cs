2c75cf434a860d1f5a7f6268e923d0022ad80f16
Starman <vicentetavareslima@gmail.com>
2025-01-27 20:31:05 +0000

    Add project files.

---
+++ Alkata/Alkata.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+namespace Alkata
+{
+	/// <summary>
+	/// Alkata is a shader container file format made to quickly expose relevant info
+	/// </summary>
+	public class Alkata
+	{
+		static readonly byte[] Magic = [ 0xFF, .. Encoding.UTF8.GetBytes("ALKATA"), 0xFF ];
+		Alkata()
+		{
+
+		}
+
+		public static Alkata Compile(Shader[] shaders, FileStream fs)
+		{
+			List<byte> buffer = [.. Magic, .. BitConverter.GetBytes(shaders.Length)];
+			
+			
+		}
+
+		public Alkata FromFile(FileStream fs)
+		{
+			Span<byte> buffer = new byte[(int)fs.Length];
+			fs.Read(buffer);
+			for (int i = 0; i < Magic.Length; i++)
+			{
+				if (buffer[i] != Magic[i])
+				{
+					throw new InvalidDataException("Invalid Alkata file");
+				}
+			}
+
+		}
+
+		static string[] GetUniforms()
+		{
+
+		}
+	}
+}
---
+++ Alkata/Shader.cs
@@ -0,0 +1,42 @@
+﻿using NetShaderc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alkata
+{ 
+
+	public struct Shader
+	{
+
+		public string Name;
+		public ShaderKind Type;
+		public string Code;
+
+
+		public Shader(FileStream fs, ShaderKind type)
+		{
+			Type = type;
+			Name = Path.GetFileName(fs.Name);	
+			Span<byte> buffer = new byte[(int)fs.Length];
+			fs.Read(buffer);
+			Code = buffer.ToString();
+
+		}
+
+		public static Shader FromFile(string path, ShaderKind type)
+		{
+			using var file = File.OpenRead(path);
+			return new(file, type);
+		}
+
+		public static Shader FromString(string code, ShaderKind type, string name = "")
+		{
+			return new Shader {Code = code, Type = type, Name = name};
+		}
+	}
+}
